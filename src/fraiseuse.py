62ca1f7030ec3f222e94108059b6f51a8f8e829e
traker <guihej@gmail.com>
2014-01-09 20:55:48 +0100

    ajout applis sans vue 2d

--- src/fraiseuse.py
+++ src/fraiseuse.py
@@ -5,13 +5,13 @@ Created on 16 oct. 2012
 '''
 
 
-class fraiseuse(object):
+class fraiseuse( object ):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params, height):
+    def __init__( self, params, height ):
         '''
         Constructor
         '''
@@ -25,76 +25,76 @@ class fraiseuse(object):
         self.axe0Y = [self.zeroMachine[0], 0, self.zeroMachine[0], self.height[0]]
 
 
-     
-    def get_posBrocheMachine(self):
-        return self.brocheMachine 
-        
-    def afficheBrocheMachine(self): # renvoi la position en mm par raport au zeroMachine
-        postemp = [(self.zeroMachine[0] - self.brocheMachine[0]) / self.mm , (self.brocheMachine[1] - self.zeroMachine[1]) / self.mm , self.brocheMachine[2] ]
-        return postemp 
 
-    
-    def get_zeroMachine(self):
+    def get_posBrocheMachine( self ):
+        return self.brocheMachine
+
+    def afficheBrocheMachine( self ): # renvoi la position en mm par raport au zeroMachine
+        postemp = [( self.zeroMachine[0] - self.brocheMachine[0] ) / self.mm , ( self.brocheMachine[1] - self.zeroMachine[1] ) / self.mm , self.brocheMachine[2] ]
+        return postemp
+
+
+    def get_zeroMachine( self ):
         return self.zeroMachine
 
-    
-    def get_posZeroVirtuel(self): # position dans le canvas
+
+    def get_posZeroVirtuel( self ): # position dans le canvas
         return self.zeroVirtuel
-    
-    def afficheZeroVirtuel(self): # renvoi la position en mm par rapport au zeroMachine
-        postemp = [(self.zeroMachine[0] - self.zeroVirtuel[0]) / self.mm , (self.zeroVirtuel[1] - self.zeroMachine[1]) / self.mm , self.zeroVirtuel[2] ]
-        return postemp       
-    
-    
-    def get_table(self):
+
+    def afficheZeroVirtuel( self ): # renvoi la position en mm par rapport au zeroMachine
+        postemp = [( self.zeroMachine[0] - self.zeroVirtuel[0] ) / self.mm , ( self.zeroVirtuel[1] - self.zeroMachine[1] ) / self.mm , self.zeroVirtuel[2] ]
+        return postemp
+
+
+    def get_table( self ):
         return self.table
-    
-    def set_axe(self, param):
+
+    def set_axe( self, param ):
         postemp = [param[0], param[1], self.brocheMachine[2]]
         self.zeroVirtuel = postemp
-        self.axe0X = [0, self.get_posZeroVirtuel()[1],self.height[1], self.get_posZeroVirtuel()[1]]
+        self.axe0X = [0, self.get_posZeroVirtuel()[1], self.height[1], self.get_posZeroVirtuel()[1]]
         self.axe0Y = [self.get_posZeroVirtuel()[0], 0, self.get_posZeroVirtuel()[0], self.height[0]]
-        
-    
-    def gohome(self):
+
+
+    def gohome( self ):
         self.brocheMachine = [self.get_posZeroVirtuel()[0], self.get_posZeroVirtuel()[1], self.get_posZeroVirtuel()[2]]
-    
-    def set_home(self):
+
+    def set_home( self ):
         self.zeroVirtuel = [self.get_posBrocheMachine()[0], self.get_posBrocheMachine()[1], self.get_posBrocheMachine()[2]]
-        self.axe0X = [0, self.get_posZeroVirtuel()[1],self.height[1], self.get_posZeroVirtuel()[1]]
+        self.axe0X = [0, self.get_posZeroVirtuel()[1], self.height[1], self.get_posZeroVirtuel()[1]]
         self.axe0Y = [self.get_posZeroVirtuel()[0], 0, self.get_posZeroVirtuel()[0], self.height[0]]
-        
-    def homeReset(self):
+
+    def homeReset( self ):
         self.zeroVirtuel = [self.get_zeroMachine()[0], self.get_zeroMachine()[1], self.get_zeroMachine()[2]]
-        self.axe0X = [0, self.get_posZeroVirtuel()[1],self.height[1], self.get_posZeroVirtuel()[1]]
+        self.axe0X = [0, self.get_posZeroVirtuel()[1], self.height[1], self.get_posZeroVirtuel()[1]]
         self.axe0Y = [self.get_posZeroVirtuel()[0], 0, self.get_posZeroVirtuel()[0], self.height[0]]
-        
-    
-    def y_movetor(self, param):
-        self.brocheMachine[0] = self.brocheMachine[0] + (param * self.mm)
-    
-    def x_movetor(self, param):
-        self.brocheMachine[1] = self.brocheMachine[1] + (param * self.mm)
-    
-    def y_movetoa(self,param):
-        self.brocheMachine[0] = self.zeroVirtuel[0] + (param * self.mm)
-    
-    def x_movetoa(self, param):
-        self.brocheMachine[1] = self.zeroVirtuel[1] - (param * self.mm)
-    
-    def get_vue_axe0X(self):
+
+
+    def y_movetor( self, param ):
+        self.brocheMachine[0] = self.brocheMachine[0] + ( param * self.mm )
+
+    def x_movetor( self, param ):
+        self.brocheMachine[1] = self.brocheMachine[1] + ( param * self.mm )
+
+    def y_movetoa( self, param ):
+        self.brocheMachine[0] = self.zeroVirtuel[0] + ( param * self.mm )
+
+    def x_movetoa( self, param ):
+        self.brocheMachine[1] = self.zeroVirtuel[1] - ( param * self.mm )
+
+    def get_vue_axe0X( self ):
         return self.axe0X
-    
-    def set_vue_axe0X(self, params):
+
+    def set_vue_axe0X( self, params ):
         self.axe0X = params
-    
-    def get_vue_axe0Y(self):
+
+    def get_vue_axe0Y( self ):
         return self.axe0Y
-    
-    def set_vue_axe0Y(self, params):
+
+    def set_vue_axe0Y( self, params ):
         self.axe0Y = params
-    
-    def set_BrocheMachine(self, param):
+
+    def set_BrocheMachine( self, param ):
         self.brocheMachine = [param[0], param[1], param[2]]
-        
+
 
